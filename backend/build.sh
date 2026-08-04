fe0291c18025cdef1d766afe4dc0f7770fb1d979
Yrdnal147 <ton_email>
2026-08-04 04:49:22 +0100

    build: configuration pour Vercel et PostgreSQL

---
+++ backend/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect static files..."
+python3.9 manage.py collectstatic --noinput
--- backend/sonoritiz_backend/settings.py
+++ backend/sonoritiz_backend/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import environ
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,22 +80,16 @@ WSGI_APPLICATION = 'sonoritiz_backend.wsgi.application'
 ASGI_APPLICATION = 'sonoritiz_backend.asgi.application'
 
 # Database configuration
-if env('DB_ENGINE') == 'django.db.backends.sqlite3':
+DATABASE_URL = env('DATABASE_URL', default=None)
+if DATABASE_URL:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
 else:
     DATABASES = {
         'default': {
-            'ENGINE': env('DB_ENGINE'),
-            'NAME': env('DB_NAME'),
-            'USER': env('DB_USER'),
-            'PASSWORD': env('DB_PASSWORD'),
-            'HOST': env('DB_HOST'),
-            'PORT': env('DB_PORT'),
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
